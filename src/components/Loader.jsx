38c275722d76c031fe14e4a11f8c28e1839fe22a
bahacan112 <bzenbil19@gmail.com>
2024-12-21 21:48:56 +0300

    asas

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,19 +1,18 @@
-import { MagnifyingGlass } from "react-loader-spinner";
+import { TailSpin } from "react-loader-spinner";
 
-const Loader = () =>
-{
-    return (
-        <MagnifyingGlass
-            visible={true}
-            height="80"
-            width="80"
-            ariaLabel="magnifying-glass-loading"
-            wrapperStyle={{}}
-            wrapperClass="magnifying-glass-wrapper"
-            glassColor="#c0efff"
-            color="#e15b64"
-        />
-    );
-}
+const Loader = () => {
+  return (
+    <TailSpin
+      visible={true}
+      height="80"
+      width="80"
+      ariaLabel="magnifying-glass-loading"
+      wrapperStyle={{}}
+      wrapperClass="magnifying-glass-wrapper"
+      glassColor="#c0efff"
+      color="#e15b64"
+    />
+  );
+};
 
-export default Loader
+export default Loader;
